2ce7b5381e76dd7b4e24e6f1fdbe200a4df54b39
Jesse Hamer <jesse.a.l.hamer@gmail.com>
2026-02-07 08:04:57 -0600

    feat(.zshenv.d/lesspipe): enable lesspipe.sh when piping from stdin

--- .zshenv.d/lesspipe.zsh
+++ .zshenv.d/lesspipe.zsh
@@ -1,8 +1,8 @@
 #!/bin/zsh
 
-LESSOPEN="|/usr/bin/lesspipe.sh %s"
 LESS="-f -R -i -m -W -F"
 LESSQUIET=1
+LESSOPEN="|-/usr/bin/lesspipe.sh %s"
 LESSCOLORIZER="bat --style=plain --color=always --theme='Catppuccin Macchiato'"
 LESSUTFCHARDEF=e000-e09f:w,e0a0-e0bf:p,e0c0-f8ff:w,f0001-fffff:w
 
