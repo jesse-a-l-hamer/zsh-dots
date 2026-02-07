27ed8639337774546b30764d2a36e6754e5bc9e1
Jesse Hamer <jesse.a.l.hamer@gmail.com>
2026-02-07 08:02:33 -0600

    style(.zshenv.d/bat): remove unneccessary comments

--- .zshenv.d/bat.zsh
+++ .zshenv.d/bat.zsh
@@ -1,7 +1,5 @@
 #!/bin/zsh
 
 export BAT_THEME="Catppuccin Macchiato"
-# export BAT_THEME_DARK="Catppuccin Macchiato"
-# export BAT_THEME_LIGHT="Catppuccin Latte"
 export BAT_PAGER="less"
 export BAT_STYLE="full"
