ee5659cf9d4ce4230d5caa381e8370637870aae1
Jesse Hamer <jesse.a.l.hamer@gmail.com>
2026-02-07 08:04:08 -0600

    feat(.zshenv.d/lesspipe): export LESS, LESSQUIET, LESSCOLORIZER, and LESSUTFCHARDEF

--- .zshenv.d/lesspipe.zsh
+++ .zshenv.d/lesspipe.zsh
@@ -1,4 +1,13 @@
 #!/bin/zsh
 
 LESSOPEN="|/usr/bin/lesspipe.sh %s"
+LESS="-f -R -i -m -W -F"
+LESSQUIET=1
+LESSCOLORIZER="bat --style=plain --color=always --theme='Catppuccin Macchiato'"
+LESSUTFCHARDEF=e000-e09f:w,e0a0-e0bf:p,e0c0-f8ff:w,f0001-fffff:w
+
+export LESS
+export LESSQUIET
 export LESSOPEN
+export LESSCOLORIZER
+export LESSUTFCHARDEF
