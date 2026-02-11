466d2ee59190d6392acac4157781c20a9e4e5aaf
Jesse Hamer <jesse.a.l.hamer@gmail.com>
2026-02-11 09:39:54 -0600

    fix(zshenv.d/keepassxc): update broken vault/key paths

--- .zshenv.d/keepassxc.zsh
+++ .zshenv.d/keepassxc.zsh
@@ -1,3 +1,3 @@
 #!/bin/zsh
-export KEEPASSXC_VAULT=$HOME/Documents/vault/keepass-jesse/keepassxc-jesse.kdbx
-export KEEPASSXC_KEY=$HOME/Documents/vault/keepass-jesse/keepassxc-jesse.key.gpg
+export KEEPASSXC_VAULT=$HOME/Documents/perennial/vault/keepass-jesse/keepassxc-jesse.kdbx
+export KEEPASSXC_KEY=$HOME/Documents/perennial/vault/keepass-jesse/keepassxc-jesse.key.gpg
